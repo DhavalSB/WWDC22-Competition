dad9f7a318457d27322713728c60c5d566ed7e31
Dhaval B <pythondsb@gmail.com>
2022-04-12 21:25:39 -0700

    Smooth Sprite Movement

--- Sources/GameScene.swift
+++ Sources/GameScene.swift
@@ -2,21 +2,80 @@ import PlaygroundSupport
 import SpriteKit
 
 class GameScene: SKScene {
+   
+    var ship: SKSpriteNode!
     
-    var bg: SKSpriteNode?
+    var cam: SKCameraNode!
 
     override func didMove(to view: SKView) {
-        bg = childNode(withName: "//Empty Space") as? SKSpriteNode
-      
-
+       
+//        ship = childNode(withName: "//Space Ship") as? SKSpriteNode
+        
+        cam = childNode(withName: "//Camera") as? SKCameraNode
     }
     
+    var leftPressed = false
+    var rightPressed = false
+    var upPressed = false
+    var downPressed = false
+    
     public override func keyDown(with event: NSEvent) {
+        switch Int(event.keyCode) {
+        case 123:
+            leftPressed = true
+        case 124:
+            rightPressed = true
+        case 125:
+            downPressed = true
+        case 126:
+            upPressed = true
+        default:
+            break
+        }
+    }
+    
+    public override func keyUp(with event: NSEvent) {
+        switch Int(event.keyCode) {
+        case 123:
+            leftPressed = false
+        case 124:
+            rightPressed = false
+        case 125:
+            downPressed = false
+        case 126:
+            upPressed = false
+        default:
+            break
+        }
     }
 
+    var lastUpdate: TimeInterval!
     override func update(_ currentTime: TimeInterval) {
+//        Smooths things out
+        defer { lastUpdate = currentTime }
+        guard lastUpdate != nil else {
+            return
+        }
         
-//        End Screen for completing game
+        let dt = currentTime - lastUpdate
+        guard dt < 1 else {
+            return //so nothing "jumps" when the the game is unpaused
+        }
+//
+        if rightPressed {
+            camera!.position.x -= 500 * dt
+        }
+        if leftPressed {
+            camera!.position.x += 500 * dt
+        }
+        if upPressed {
+//            ship.position.y += 100 * dt
+        }
+        if downPressed {
+//            ship.position.y -= 100 * dt
+        }
+        
+
     }
     
     @objc public static override var supportsSecureCoding: Bool {
@@ -26,3 +85,9 @@ class GameScene: SKScene {
     }
 }
  
+func moveSprite (node : SKNode, moveByX: CGFloat, moveByY: CGFloat, forTheKey: String, duration: TimeInterval) {
+    let moveAction = SKAction.moveBy(x: moveByX, y: moveByY, duration: duration)
+    let seq = SKAction.sequence([moveAction])
+    
+    node.run(seq, withKey: forTheKey)
+    }
